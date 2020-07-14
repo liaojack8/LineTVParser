5758872e531a3ed3e9ba845434e4d20267eeeb57
liaojack8 <liaojack8@yahoo.com.tw>
2020-07-15 01:08:26 +0800

    add func to download tools automatically

--- LineTVParser.py
+++ LineTVParser.py
@@ -1,43 +1,61 @@
 import os
+import shutil
 from tqdm import tqdm
+import zipfile
 import json
 import requests
 forceHD = True
-
+currentPath = ''
 def init():
+	global currentPath
+	currentPath = os.getcwd().replace('\\','/')
 	if not os.path.isdir('./bin'):
 		os.mkdir('./bin')
 	if not os.path.isdir('./download'):
 		os.mkdir('./download')
 	if not os.path.isfile('./bin/youtube-dl.exe'):
-		print('youtube-dl not found!\nDownloading...')
-		r = requests.get('https://yt-dl.org/downloads/latest/youtube-dl.exe', stream=True)
-		is_chunked = r.headers.get('transfer-encoding', '') == 'chunked'
-		content_length_s = r.headers.get('content-length')
-		if not is_chunked and content_length_s.isdigit():
-			content_length = int(content_length_s)
-		else:
-			content_length = None
-		pbar = tqdm(total=content_length, unit="B", unit_scale=True, ncols=100)
-		with open('./bin/youtube-dl.exe', 'wb') as f:
-			for chunk in r.iter_content(chunk_size=1024 * 1024):
-				f.write(chunk)
-				pbar.update(len(chunk))
-			pbar.close()
-		f.close()
+		print('[SysInfo:]youtube-dl not found!\nDownloading...')
+		dlFile('https://yt-dl.org/downloads/latest/youtube-dl.exe', './bin/youtube-dl.exe')
+	if not os.path.isfile('./bin/ffmpeg.exe'):
+		print('[SysInfo:]ffmpeg not found!\nDownloading...')
+		dlFile('https://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-latest-win32-static.zip', './bin/ffmpeg-latest-win32-static.zip')
+		print('Unziping...')
+		with zipfile.ZipFile('./bin/ffmpeg-latest-win32-static.zip', 'r') as zip_ref:
+			zip_ref.extractall('./bin')
+		os.remove('./bin/ffmpeg-latest-win32-static.zip')
+		shutil.move('./bin/ffmpeg-latest-win32-static/bin/ffmpeg.exe', "./bin/ffmpeg.exe")
+		shutil.rmtree('./bin/ffmpeg-latest-win32-static')
+
+def dlFile(url, path):
+	r = requests.get(url, stream=True)
+	is_chunked = r.headers.get('transfer-encoding', '') == 'chunked'
+	content_length_s = r.headers.get('content-length')
+	if not is_chunked and content_length_s.isdigit():
+		content_length = int(content_length_s)
+	else:
+		content_length = None
+	pbar = tqdm(total=content_length, unit="B", unit_scale=True, ncols=100)
+	with open(path, 'wb') as f:
+		for chunk in r.iter_content(chunk_size=1024 * 1024):
+			f.write(chunk)
+			pbar.update(len(chunk))
+		pbar.close()
+	f.close()
 
 def welcome():
-	VideoLink = 'https://www.linetv.tw/drama/10900/eps/1'#input("Video Link: ")
-	return [VideoLink.split('/')[-1], VideoLink.split('/')[-3]] #episode, dramaID
+	VideoLink = input("Video Link: ")
+	episode, dramaID = VideoLink.split('/')[-1], VideoLink.split('/')[-3]
+	return [dramaID, episode]
 
 def getInfo(ary):
-	r = requests.get("https://www.linetv.tw/api/part/"+ary[1]+"/eps/"+ary[0]+"/part")
+	r = requests.get("https://www.linetv.tw/api/part/"+ary[0]+"/eps/"+ary[1]+"/part")
 	resJson = json.loads(r.content.decode())
-	print('[DramaInfo:]',resJson['dramaInfo']["name"]+' EP'+str(resJson['dramaInfo']["eps"])+' '+str(resJson['dramaInfo']["year"]))
+	dramaName = resJson['dramaInfo']["name"]
+	print('[DramaInfo:]',dramaName+' EP'+ary[1]+' '+str(resJson['dramaInfo']["year"]))
 	if resJson["epsInfo"]["source"][0]["links"][0]["subtitle"] != None:
 		getSubtitle(resJson["epsInfo"]["source"][0]["links"][0]["subtitle"], './download/'+resJson['dramaInfo']["name"]+'_EP'+str(resJson['dramaInfo']["eps"])+'.srt')
-	#m3u8Url, keyType, keyId, dramaID
-	return [ary, resJson["epsInfo"]["source"][0]["links"][0]["link"], resJson["epsInfo"]["source"][0]["links"][0]["keyType"], resJson["epsInfo"]["source"][0]["links"][0]["keyId"]]
+	m3u8Url, keyType, keyId = resJson["epsInfo"]["source"][0]["links"][0]["link"], resJson["epsInfo"]["source"][0]["links"][0]["keyType"], resJson["epsInfo"]["source"][0]["links"][0]["keyId"]
+	return [ary, m3u8Url, keyType, keyId, dramaName]
 
 def getSubtitle(subtitleLink, path):
 	r = requests.get(subtitleLink, stream=True)
@@ -59,16 +77,20 @@ def getToken(ary):
 	queryjson = {
 	'keyType':ary[2],
 	'keyId':ary[3],
-	'dramaId':ary[0][1],
-	'eps':ary[0][0]
+	'dramaId':ary[0][0],
+	'eps':ary[0][1]
 	}
 	r = requests.post("https://www.linetv.tw/api/part/dinosaurKeeper", json = queryjson)
 	resJson = json.loads(r.content.decode())
+	return [ary, 'authentication:' + resJson['token']]
+
+def getFile(ary):
+	global currentPath
 	if forceHD:
-		return [ary[1].replace('SD','HD'), 'authentication:' + resJson['token']]
+		url = ary[0][1].replace('SD','HD')
 	else:
-		return [ary[1], 'authentication:' + resJson['token']]
+		url = ary[0][1]
+	os.system(currentPath + '/bin/youtube-dl.exe --add-header ' + '"' + str(ary[1]) + '" ' + url +' --output ./download/"' + str(ary[0][4]+'_EP'+ary[0][0][1]) + '.mp4"')
 
-
-# init()
-print(getToken((getInfo(welcome()))))
+init()
+getFile(getToken((getInfo(welcome()))))
